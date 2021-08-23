78f316b2a9ed1bd6909b24230d6f5897f77b693f
Hemendra Chaudhary <hemendrachaudhary2052000@gmail.com>
2021-08-23 21:35:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const date = require(__dirname + "/date.js")
+
+const items = [];
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+app.get("/", function(req, res) {
+
+  let day = date.getDate()
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  })
+});
+
+app.post("/", function(req, res){
+  const item = req.body.newItem
+  items.push(item)
+
+  res.redirect("/")
+})
+
+app.listen(3000, function() {
+  console.log("Server is up & listening on 3000");
+})
